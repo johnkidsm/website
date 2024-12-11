c6213ebf5c4d48abadde30bfb4a850e1121b1588
johnnykindermann <106040053+johnnykindermann@users.noreply.github.com>
2024-12-11 16:02:36 +0100

    adjustment 9

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,7 +24,7 @@ const Hero = () => {
            <p className={`text-[18px] mt-2 text-white-100`}>
            In a world increasingly driven by technology, <br />
            VirtualAI China emerges as a visionary company at the forefront <br />
-           of virtual intelligence: artificial intelligence, virtual reality and augmented reality(AI + XR).
+           of "virtual intelligence": artificial intelligence, virtual reality and augmented reality(AI + XR).
            </p>
         </div>
       </div>
