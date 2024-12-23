d5340a73e25d64e5c220d7a3c908d0a51f086673
johnnykindermann <106040053+johnnykindermann@users.noreply.github.com>
2024-12-23 11:42:47 +0100

    adjustment 9

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <p className="text-sm text-gray-400 font-light tracking-wide">
         Address: 59 East Yan'an Road, Waitan, Huangpu District, Shanghai 邮政编码：200000
       </p>
-      <div className="flex flex-col justify-between items-center w-full h-20"></div> 
+      <div className="flex flex-row justify-between items-center w-20"> 
       <a 
         href="https://www.linkedin.com/company/virtualai-china/"
         target="_blank"
@@ -22,6 +22,37 @@ const Footer = () => {
           className="opacity-70 rounded hover:opacity-100 transition-opacity"
         />
       </a>
+      <a 
+        href="https://www.linkedin.com/company/virtualai-china/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="my-2"
+        aria-label="Visit VirtualAI China on Instagram"
+      >
+        <img
+          src="/instagram.jpg"
+          alt="Instagram"
+          width={25}
+          height={25}
+          className="opacity-70 rounded hover:opacity-100 transition-opacity"
+        />
+      </a>
+      <a 
+        href="https://www.linkedin.com/company/virtualai-china/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="my-2"
+        aria-label="Visit VirtualAI China on Twitter"
+      >
+        <img
+          src="/twitter.png"
+          alt="Twitter"
+          width={25}
+          height={25}
+          className="opacity-70 rounded hover:opacity-100 transition-opacity"
+        />
+      </a>
+      </div>
       <p className="text-sm text-gray-400 font-light tracking-wide">
         VirtualAI China © 2024
       </p>
